e73d91d502c5bcb6a6476bc97edc79e2985f1ef2
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-11-29 19:02:05 -0300

    feat(Page: Dashboard): :poop: problema com dynamic

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,35 @@
-import { Flex } from "@chakra-ui/react";
+import dynamic from 'next/dynamic'
+
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
+
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
-export default function Dashboard(){
-    return <Flex direction="column" h="100vh">
-        <Header></Header>
-        <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-<Sidebar></Sidebar>
+import Chart = dynamic(()=>import('react-apexcharts'), {ssr: false});
+
+const options = {};
+const series = [{ name: "series1", data: [31, 120, 10, 28, 51, 18, 109] }];
+
+export default function Dashboard() {
+    return (
+        <Flex direction="column" h="100vh">
+            <Header></Header>
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar></Sidebar>
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+                    <Box p="8" bg="gray.800" borderRadius={8}>
+                        <Text fontSize="lg" mb="4">
+                            Inscritos da semana
+                        </Text>
+                        <Chart type="area" heigth={160} options={options} series={series}></Chart>
+                    </Box>
+                    <Box p="8" bg="gray.800" borderRadius={8}>
+                        <Text fontSize="lg" mb="4">
+                            Taxa de abertura
+                        </Text>
+                    </Box>
+                </SimpleGrid>
+            </Flex>
         </Flex>
-    </Flex>
+    );
 }
