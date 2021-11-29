e4f52d14b2bc1b4678aa3612ba1c6d198d34e8d9
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-11-29 17:59:03 -0300

    feat(Componente: Input): :sparkles: Criando componente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,13 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Input } from '../components/Form/Input';
 
 export default function Home() {
     return (
         <Flex w="100vw" h="100vh" align="center" justify="center">
             <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
                 <Stack spacing="4">
-                    <FormControl>
-                        <FormLabel htmlFor="email">E-mail</FormLabel>
-                        <Input
-                            name="email"
-                            type="email"
-                            _hover={{ bgColor: "gray.900" }}
-                            focusBorderColor="pink.500"
-                            bg="gray.900"
-                            variant="filled"
-                            size="lg"
-                        ></Input>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel htmlFor="password">Password</FormLabel>
-                        <Input
-                            name="password"
-                            type="password"
-                            focusBorderColor="pink.500"
-                            bg="gray.900"
-                            variant="filled"
-                            _hover={{ bgColor: "gray.900" }}
-                            size="lg"
-                        ></Input>
-                    </FormControl>
+                    <Input name="email" type="email" label="E-mail"/>
+                    <Input name="password" type="password" label="Password"/>
                 </Stack>
                 <Button type="submit" mt="6" colorScheme="pink" size="lg">
                     Entrar
