8a3c881482e5933f3ea03c6c9ebf2cdd6000e657
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-11-30 17:17:58 -0300

    feat(Component: Pagination): :sparkles: Criação do componente

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -63,6 +64,7 @@ export default function UserList() {
                             </Tr>
                         </Tbody>
                     </Table>
+                    <Pagination></Pagination>
                 </Box>
             </Flex>
         </Box>
