83a56036e69b79842e19e102f9d79bf26db265b8
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-01-21 17:44:57 -0300

    feat: :sparkles: Form de autenticação

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,23 +1,26 @@
 import { FormLabel, Input as ChakraInput, FormControl, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
     return (
         <FormControl>
             {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
             <ChakraInput
+                ref={ref}
                 name={name}
                 _hover={{ bgColor: "gray.900" }}
                 focusBorderColor="pink.500"
                 bg="gray.900"
                 variant="filled"
                 size="lg"
-                {...rest}
-            ></ChakraInput>
+                {...rest}></ChakraInput>
         </FormControl>
     );
-}
+};
+
+export const Input = forwardRef(InputBase);
